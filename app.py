e21f0e81d215a2ad8fd99f12b3eb4337af6285de
harryphelps2 <harryphelps1@gmail.com>
2018-08-18 18:11:19 +0100

    Include instructios and accordion version for smaller screens

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -14,6 +15,16 @@ mongo = PyMongo(app)
 def get_recipes():
     return render_template("recipes.html", 
     recipes=mongo.db.recipes.find())
+
+@app.route('/<oid>')
+def cook_recipe(oid):
+    return render_template("recipe.html",
+    recipe=mongo.db.recipes.find({"_id": ObjectId(oid)}))
+
+@app.route('/steps/<oid>')
+def cook_recipe_fullscreen(oid):
+    return render_template("steps.html",
+    recipe=mongo.db.recipes.find({"_id": ObjectId(oid)}))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
